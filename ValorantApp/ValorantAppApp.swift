647c26d47581394b9b5e9904508d9b92989e86a5
Omer Erbalta <erbalta80@gmail.com>
2023-10-18 19:23:23 +0300

    Initial Commit

---
+++ ValorantApp/ValorantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantAppApp.swift
+//  ValorantApp
+//
+//  Created by OmerErbalta on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ValorantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
